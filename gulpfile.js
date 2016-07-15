d073ef2c35dbf4af4af39dab0ad62482eaeaff8f
Katherine Beame <katherine.beame@gmail.com>
2016-07-15 11:48:55 -0700

    working server and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,17 @@ const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 
 var files = ['*.js', './lib/**/*.js', './routes/**/*.js', './models/**/*.js'];
+var testFiles = ['./test/**/*.js'];
+
+gulp.task('lint', () => {
+  gulp.src(files)
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('test:mocha', () => {
+  return gulp.src(testFiles)
+    .pipe(mocha());
+});
+
+gulp.task('default', ['lint', 'test:mocha']);
--- server.js
+++ server.js
@@ -1,22 +1,6 @@
-describe('cowsaycli', function() {
-  before(function() {
-    this.argv_backup = process.argv;
-    process.argv = [null, null, 'test'];
-  });
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-  after(function() {
-    process.argv = this.argv_backup;
-  });
 
-  it('should read from process.argv', function() {
-    var called = false;
-    var testStream = {
-      write: function(input) {
-        called = true;
-        expect(input).to.eql(cowsay('test'));
-      }
-    };
-    cowsaycli(undefined, testStream);
-    expect(called).to.eql(true);
-  });
-});
+module.exports = app.listen(PORT, () => console.log('server up at port ' + PORT));
